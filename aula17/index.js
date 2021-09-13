aa73988f946d81de0bec952694130ee5657c7bec
Erick Fernandes <erick.farias@dcx.ufpb.br>
2021-09-13 08:09:50 -0300

    Curso Javascript e Typescript (FULL-STACK)

---
+++ aula17/index.js
@@ -0,0 +1,36 @@
+const pessoas = [
+    {
+        "nome": "Erick",
+        "idade": 21
+    }
+];
+
+console.log(pessoas[0].nome);
+
+//CRIAR UM ARRAY COM UM TIPO DE DADO
+
+
+//               0       1        2    
+const alunos = ['Luiz', 'Maria', 'João'];
+
+alunos[0] = 'Eduardo';//vai substituir Luiz por Eduardo
+alunos[3] = 'Luiza';// vai adcionar o nome Luiza do indice 3
+
+console.log(alunos.length)// tamanho do array
+
+alunos[alunos.length] = "Fernando";
+alunos[alunos.length] = "Luana";
+alunos[alunos.length] = "Fábio";
+
+
+console.log(alunos);
+
+
+
+
+
+
+
+
+
+
