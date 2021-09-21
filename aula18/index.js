9851208f1f0bc00f4f872c9b979d0f5f06c2b17f
Erick Fernandes <erick.farias@dcx.ufpb.br>
2021-09-21 13:54:49 -0300

    Curso Javascript full-stack

---
+++ aula18/index.js
@@ -0,0 +1,12 @@
+// function saudacao() {
+//     const nome = "Hello worl";
+//     return nome;
+// }
+
+// console.log(saudacao())
+
+function saudacao(nome) {
+    console.log(`Bom dia ${nome}`)
+}
+
+saudacao("Erick")
