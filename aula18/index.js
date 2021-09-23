b608835d5abdd74b17a8800e165fc3b297ed468e
Erick Fernandes <erick.farias@dcx.ufpb.br>
2021-09-23 12:52:15 -0300

    curso Javascript

--- aula18/index.js
+++ aula18/index.js
@@ -5,8 +5,37 @@
 
 // console.log(saudacao())
 
-function saudacao(nome) {
-    console.log(`Bom dia ${nome}`)
+
+// saudacao("Erick")
+// saudacao("José")
+// saudacao("Maria")
+
+// let fumcaoArrow = (nome) => {
+    //     return `Seja bem vindo ${nome}`
+    // }
+    
+    // let nomes = function(nome) {
+        
+        // }
+
+
+// função que retorna uma string
+// function saudacao(nome) {
+//     return `Bom dia ${nome}!`
+// }
+
+// let variavel = saudacao("Luiz");
+
+// console.log(variavel)
+
+
+// função que retorna uma adição
+function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
 }
 
-saudacao("Erick")
+console.log(soma(2, 2));
+console.log(soma(3, 1));
+console.log(soma(5, 10));
+
